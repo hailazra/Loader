b0a164bea00c3f6386a7c6f3ddeaa34c8a58d8d6
hailazra <arabaniseis@gmail.com>
2025-06-27 18:43:35 +0700

    Update in development.lua

--- in development.lua
+++ in development.lua
@@ -1,4 +1,4 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local OrionLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/jensonhirst/Orion/main/source")))()
 local Window = OrionLib:MakeWindow({Name = "Astro | In Development", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
 local FarmTab = Window:MakeTab({
